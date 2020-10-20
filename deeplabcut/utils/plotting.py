0ad68bddec900985d0dddc1c573bc8c482817bbb
Alex Smith <smith@horde-kennyLab.hcsm.mssm.edu>
2020-10-20 12:26:14 -0400

    Added optional arguments for plot_trajectories

--- deeplabcut/utils/plotting.py
+++ deeplabcut/utils/plotting.py
@@ -31,7 +31,7 @@ def Histogram(vector, color, bins, ax=None, linewidth=1.0):
         ax = fig.add_subplot(111)
     ax.hist(dvector, color=color, histtype="step", bins=bins, linewidth=linewidth)
 
-
+             
 def PlottingResults(
     tmpfolder,
     Dataframe,
@@ -209,6 +209,15 @@ def plot_trajectories(
 
     destfolder: string, optional
         Specifies the destination folder that was used for storing analysis data (default is the path of the video).
+        
+    imagetype: string, default ".png"
+        Specifies the output image format, tested '.tif', '.jpg', '.svg'
+
+    resolution: int, default 100
+        Specifies the resolution (in dpi) of saved figures. Note higher resolution figures take longer to generate.
+        
+    linewidth: float, default 1.0
+        Specifies width of line for line and histogram plots.
 
     imagetype: string, default ".png"
         Specifies the output image format, tested '.tif', '.jpg', '.svg' and ".png". 

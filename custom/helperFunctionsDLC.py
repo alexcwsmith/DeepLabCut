72d3030c3ae6541af98c5a97728d8287c4d8692e
Alex Smith <alexander.smith@mssm.edu>
2021-03-09 16:59:52 -0500

    Added social interaction helper functions

--- custom/helperFunctionsDLC.py
+++ custom/helperFunctionsDLC.py
@@ -11,6 +11,9 @@ import pandas as pd
 import os
 import shutil
 import cv2
+from scipy.stats import ttest_ind
+from statsmodels.stats.multitest import multipletests
+import matplotlib.pyplot as plt
 
 
 def h5toCSV(directory):
@@ -21,6 +24,30 @@ def h5toCSV(directory):
             fullpath=os.path.join(directory, f)
             df = pd.read_hdf(fullpath)
             df.to_csv(os.path.join(directory, n + '.csv'))
+
+def interpolateValues(file, pcutoff):
+    df = pd.read_csv(file, index_col=0, skiprows=[0], header=[0,1])    
+    bodyparts = set(df.columns[x][0] for x in range(len(df.columns)))
+    for bp in bodyparts:
+        df.loc[df[bp, 'likelihood'] < pcutoff, (bp, 'x')]=None
+        df.loc[df[bp, 'likelihood'] < pcutoff, (bp, 'y')]=None
+    df = df.interpolate(method='linear')
+    df = df.dropna(how='any', axis=0)
+    filename = file.strip('.csv')
+    df.to_csv(filename + '_interpolated.csv')
+    df.to_hdf(filename + '_interpolated.h5', key='df_with_missing')
+    return(df)
+
+def trimCSVs(frames, directory):
+    files = os.listdir(directory)
+    saveDir = os.path.join(directory, 'trimmedCSVs/')
+    for f in files:
+        if f.endswith('.csv'):
+            sampleName = f.split('DLC')[0]
+            fullpath = os.path.join(directory, f)
+            df = pd.read_csv(fullpath, index_col=0)
+            df = df[:frames+2]
+            df.to_csv(os.path.join(saveDir, sampleName + '_trimmed.csv'))
 
 def extractPoses(parentDirectory, prefix='VG'):
     """Extract and re-organizes files from plot_poses (result of dlc.plot_trajectories) into folders for each plot type.
@@ -259,7 +286,6 @@ def makeZoneVideo(csvPath, modelPrefix, bodyPart, axis, frameDir, fps, size, fli
             else:
                 pass
 
-    left, right, leftIndex, rightIndex = calcZoneTimes(csvPath, modelPrefix, flippedX=flippedX, index=True)
     left_img_array = []
     right_img_array = []
     if not os.path.exists(os.path.join(frameDir, sampleName + '_LeftZone.mp4')):
@@ -352,5 +378,113 @@ def countBouts(csvPath, modelPrefix, bodyPart, axis='x', saveDir=None, flippedX=
     if saveDir:
         df.to_csv(os.path.join(saveDir, sampleName + '_ZoneBouts.csv'))
     return df    
+
+def calcDistanceMA(h5File, indiv1, indiv2, bodyPart1, bodyPart2, directory=os.getcwd(), distThreshold=5):
+    """Calculate the distance between two bodyparts on two individuals in multi-animal DLC.
+
+    Parameters
+    ----------
+    h5File : string
+        Path to h5 file containing tracking data.
+    indiv1 : string
+        Label of first individual (e.g. mouse1)
+    indiv2 : string
+        Label of second individual (e.g. mouse2)
+    bodyPart1 : string
+        Body part of first individual (e.g. nose)
+    bodyPart2 : string
+        Body part of second individual (e.g. nose)
+    directory : string (optional)
+        Directory to save to. Default current working directory.
+    distThreshold : int/float (optional)
+        Pixel distance threshold to be considered interacting. The default is 5.
+
+    Returns
+    -------
+    Pandas dataframe of interactions.
+
+    """
+    df = pd.read_hdf(h5File)
+    sampleName = h5File.split('/')[-1].split('DLC')[0]
+    scorerName = df.columns[0][0]
+    bp1x = df[(scorerName, indiv1, bodyPart1, 'x')]
+    bp1x.interpolate(method='linear', inplace=True)
+    bp1y = df[(scorerName, indiv1, bodyPart1, 'y')]
+    bp1y.interpolate(method='linear', inplace=True)
+    bp2x = df[(scorerName, indiv2, bodyPart2, 'x')]
+    bp2x.interpolate(method='linear', inplace=True)
+    bp2y = df[(scorerName, indiv2, bodyPart2, 'y')]
+    bp2y.interpolate(method='linear', inplace=True)
+    bp1coords = list(zip(bp1x.tolist(), bp1y.tolist()))
+    bp2coords = list(zip(bp2x.tolist(), bp2y.tolist()))
+    dists = []
+    for i in range(len(bp1coords)):
+        d = np.linalg.norm(np.array(bp1coords[i])-np.array(bp2coords[i]))
+        dists.append(d)
+    distDf = pd.DataFrame([bp1coords,bp2coords,dists]).T
+    distDf.columns=[indiv1+'_'+bodyPart1, indiv2+'_'+bodyPart2, 'Distance']
+    distDf.to_csv(os.path.join(directory, sampleName + '_EuclideanDistances.csv'))
+    interactions = distDf.loc[distDf['Distance']<distThreshold]
+    interactions.to_csv(os.path.join(directory, sampleName + '_DistThreshold' + str(distThreshold) + '_Interactions.csv'))
+    return(interactions)
+
+def compareAndPlotInteractions(directory, group1, group2):
+    """Compare results from calcMAinteractions with a t-test, and create bar graph of two groups.
+
+    Parameters
+    ----------
+    directory : string (optional)
+        Directory containing interaction data.
+    group1 : list of strings
+        Trial IDs for group 1.
+    group22 : string
+        Trial IDs for group 2.
+
+    Returns
+    -------
+    Pandas dataframe of results. Also saves bar graph.
+
+    """
+    
+    files = os.listdir(directory)
+    samplesG1 = []
+    countsG1 = []
+    samplesG2 = []
+    countsG2 = []
+    for f in files:
+        if f.endswith('_Interactions.csv'):
+            sampleName = f.split('/')[0].split('_Dist')[0]
+            df = pd.read_csv(os.path.join(directory, f), index_col=0)
+            count = df.shape[0]
+            if sampleName in group1:
+                samplesG1.append(sampleName)
+                countsG1.append(count)
+            elif sampleName in group2:
+                samplesG2.append(sampleName)
+                countsG2.append(count)
+                
+    g1_arr = np.array(countsG1)
+    g2_arr = np.array(countsG2)
+    g1mean = np.mean(countsG1)
+    g2mean = np.mean(countsG2)
+    g1sem = np.std(countsG1)/np.sqrt(len(countsG1))
+    g2sem = np.std(countsG2)/np.sqrt(len(countsG2))
+    err = [g1sem, g2sem]
+    pval = np.array([ttest_ind(g1_arr, g2_arr, nan_policy='omit')])[0][1]
+    fdr_pass,qvals,_,_ = multipletests(pval, method='fdr_bh',alpha=0.05)
+    
+    res = [g1mean, g2mean, g1sem, g2sem, pval]
+    resNames = ['Group1_Mean', 'Group2_Mean', 'Group1_SEM', 'Group2_SEM', 't-test p-value']
+    result = pd.DataFrame([samplesG1, countsG1, samplesG2, countsG2, resNames, res]).T
+    result.columns=['Trial_Group1', 'InteractionCount_Group1', 'Trial_Group2', 'InteractionCount_Group2', 'Statistic', 'Value']
+    result.to_csv(os.path.join(directory, 'Combined_InteractionCounts.csv'))
+
+    plt.figure()
+    plt.bar(['Group1','Group2'],[g1mean, g2mean])
+    plt.errorbar(['Group1', 'Group2'], [g1mean, g2mean], yerr=err, fmt='o', color='r')
+    plt.ylabel('# Frames Interacting', fontsize=16)
+    plt.savefig(os.path.join(directory, 'InteractionFrames.png'))
+    plt.show()
+    return(result)
 
 
